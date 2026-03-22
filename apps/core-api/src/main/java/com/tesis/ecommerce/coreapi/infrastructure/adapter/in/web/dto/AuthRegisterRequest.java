97ab4acdb9792fb557da99db8b1790a0d3318c79
ChristopherPalloArias <cipallo@uce.edu.ec>
2026-03-22 12:20:26 -0500

    fix(core-api): prevent 500 on auth register

--- apps/core-api/src/main/java/com/tesis/ecommerce/coreapi/infrastructure/adapter/in/web/dto/AuthRegisterRequest.java
+++ apps/core-api/src/main/java/com/tesis/ecommerce/coreapi/infrastructure/adapter/in/web/dto/AuthRegisterRequest.java
@@ -1,5 +1,7 @@
 package com.tesis.ecommerce.coreapi.infrastructure.adapter.in.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -8,8 +10,15 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class AuthRegisterRequest {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+    
+    @NotBlank(message = "Password is required")
     private String password;
+    
+    @NotBlank(message = "Name is required")
     private String name;
 }
 
